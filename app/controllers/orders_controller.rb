585f3fbec5b8a0c54b5cd53289ed6bc8b28ed85c
Peter <psynevskyy.com>
2019-06-14 19:49:15 +0000

    added extra items to email

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
-      print UserMailer.welcome_email(order).deliver_later
+      UserMailer.welcome_email(order).deliver
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,11 @@
 class UserMailer < ApplicationMailer
 	default from: 'no-reply@jungle.com'
- 
+
   def welcome_email(order)
     @order = order
     @url  = 'http://localhost:3000/login'
 
-   #  def enhanced_cart
-   #  	@enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
-	  # end
-	  # helper_method :enhanced_cart
-
-	  # def cart_subtotal_cents
-	  #   enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
-	  # end
-	  # helper_method :cart_subtotal_cents
-
-    mail(to: order.email, subject: 'Thank you for the order! #{@order.id}')
+    mail(to: @order.email, subject: 'Thank you for the order! #{@order.id}')
   end
 
 end
