c1317c1e981f2d54202a17666b26ace70d8cab40
Peter <psynevskyy.com>
2019-06-16 01:05:04 +0000

    delete reviews

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,23 +1,36 @@
 class ReviewController < ApplicationController
-	before_filter :create
+	before_filter :create, :destroy
 
 	def create
 		@review = Review.new(review_params)
+		@review.user_id = current_user
 		@review.product_id = params[:product_id]
-		@review.user_id = current_user.id
+		
 
   	if @review.save
-  		redirect_to '/'
+  		# redirect_to '/'
+  		redirect_to "/products/#{params[:product_id]}"
   	else
   		p @review.errors
   		redirect_to :back
   	end	
 
-  	# private
 	end
-
 	def review_params
       params.require(:review).permit(:description, :rating)
   end
+
+	def destroy
+		# @product = Product.find (params[:product_id])
+		# @review_item = Review.find (params[:id])
+		# @review_item.destroy
+		# redirect_to '/'
+		@review = Review.find params[:id]
+    @review.destroy
+    redirect_to "/products/#{params[:product_id]}"
+	end
+
+	
+	
 
 end
