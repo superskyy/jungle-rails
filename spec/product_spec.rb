b4d6e61007128c669de21325ef0e5a0de9c794ef
Peter <psynevskyy.com>
2019-06-17 21:03:56 +0000

    created seperate test for each validatiidation

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -12,7 +12,8 @@ RSpec.describe Product, type: :model do
 
   	it 'should have a name' do
   		@product = Product.new name: "Macbook"
-			# @product.save!
+			@product.save
+			@product.errors.full_messages
 
 			expect(@product.name).to be_present
     	
@@ -20,6 +21,8 @@ RSpec.describe Product, type: :model do
 
   	it 'should have a price' do
   		@product = Product.new price: 10
+  		@product.save
+  		@product.errors.full_messages
 
 			expect(@product.price).to be_present
     	
@@ -27,13 +30,19 @@ RSpec.describe Product, type: :model do
 
   	it 'should have a quantity' do
   		@product = Product.new quantity: 50
+  		@product.save
+  		@product.errors.full_messages
 
 			expect(@product.quantity).to be_present
   	end
 
   	it 'should have a category' do
   		@category = Category.new name: "Computers"
+  		@category.save
+  		@category.errors.full_messages
   		@product = Product.new category: @category
+  		@product.save
+  		@product.errors.full_messages
 
 			expect(@product.category).to be_present
   	end
